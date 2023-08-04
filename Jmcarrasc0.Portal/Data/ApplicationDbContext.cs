80c368073d101a4976015590eb442f0f7b0ba585
Juan Carrasco <j_mcarrasco@outlook.com>
2023-08-04 19:04:16 -0400

    Agregar archivos de proyecto.

---
+++ Jmcarrasc0.Portal/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jmcarrasc0.Portal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
