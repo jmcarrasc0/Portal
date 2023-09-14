62e8db6b36a917a54d58f1324d5fa2a83f75478c
Juan Carrasco <j_mcarrasco@outlook.com>
2023-09-13 23:35:39 -0400

    Login

--- Jmcarrasc0.Portal/Program.cs
+++ Jmcarrasc0.Portal/Program.cs
@@ -5,6 +5,7 @@ using Jmcarrasc0.Portal.Data;
 using Jmcarrasc0.Portal.Models;
 using Jmcarrasc0.Portal.Models.Validations;
 using Jmcarrasc0.Portal.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
@@ -16,14 +17,31 @@ using System.Net.Http.Headers;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<PortalContext>(o =>o.UseSqlServer(builder.Configuration["ConnectionStrings:Portal"]));
+builder.Services.AddDbContext<PortalContext>(o => o.UseSqlServer(builder.Configuration["ConnectionStrings:Portal"]));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+builder.Services.AddAuthentication(
+    o =>
+    {
+        o.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    }).AddCookie(
+    r =>
+    {
+        r.SlidingExpiration = true;
+        r.Cookie.Name = "jmcarrasc0.Session";
+        r.LoginPath = new PathString("/Account/Login");
+        r.LogoutPath = new PathString("/Account/LogOff");
+        r.AccessDeniedPath = new PathString("/Account/CallUnauthorized");
+        r.SlidingExpiration = true;
+        r.ExpireTimeSpan = TimeSpan.FromMinutes(15);
+        r.Cookie.SameSite = SameSiteMode.None;
+    });
 
-builder.Services.AddSignalR(e =>{e.MaximumReceiveMessageSize = 102400000;});
+
+builder.Services.AddSignalR(e => { e.MaximumReceiveMessageSize = 102400000; });
 
 builder.Services.AddScoped<Mensajeria, Mensajeria>();
 builder.Services.AddScoped<LoadingServices>();
